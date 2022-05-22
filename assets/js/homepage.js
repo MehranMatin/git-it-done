e318b0205003b6d5a6f97a7a0745029f9ffe8c22
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-05-22 12:23:00 -0400

    added form handler

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,25 @@
+/* Form Handler */
+var userFormEl = document.querySelector('#user-form');
+var nameInputEl = document.querySelector('#username');
+
+var formSubmitHandler = function(event) {
+    // stops the browser from sending the form's input data to a URL
+    event.preventDefault();
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    // if there is in fact a value to username...
+    if (username) {
+        // pass input element's value data as an argument
+        getUserRepos(username);
+        // clear form input
+        nameInputEl.value = '';
+    } else {
+        alert('Please enter a GitHub username');
+    }
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = 'https://api.github.com/users/' + user + '/repos';
@@ -11,4 +33,4 @@ var getUserRepos = function(user) {
     });
 };
 
-getUserRepos('octocat');
+userFormEl.addEventListener('submit', formSubmitHandler);

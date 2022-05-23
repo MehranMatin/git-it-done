d17ccbe405736fde4009a1f67fc4e32d0a851620
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-05-23 17:49:17 -0400

    icons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -29,7 +29,6 @@ var getUserRepos = function(user) {
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
-        console.log(response);
         response.json().then(function(data) {
             displayRepos(data, user);
         });
@@ -59,6 +58,20 @@ var displayRepos = function(repos, searchTerm) {
 
         // append to container
         repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement('span');
+        statusEl.classList = 'flex-row align-center';
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
 
         // append container to the DOM
         repoContainerEl.appendChild(repoEl);
